7431a648bf843312e16c25f3ec841e4acf6aadfb
r33drichards <rwendt1337@gmail.com>
2025-01-09 12:53:13 -0800

    vc

--- flake.nix
+++ flake.nix
@@ -1,17 +1,24 @@
-{ config, pkgs, ... }:
-
 {
-  packages.reverse-proxy = nixos-generators.nixosGenerate {
-    system = "x86_64-linux";
-    format = "amazon";
-    modules = [
-      ({ modulesPath, ... }: {
-        imports = [
-          "${modulesPath}/virtualisation/amazon-image.nix"
-        ];
-      })
-      ./configuration.nix
-      ({ ... }: { amazonImage.sizeMB = 16 * 1024; })
-    ];
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixos-generators = {
+    url = "github:nix-community/nixos-generators";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
+
+  outputs = { nixpkgs, nixos-generators, ... }:
+  {
+    packages.reverse-proxy = nixos-generators.nixosGenerate {
+      system = "x86_64-linux";
+      format = "amazon";
+      modules = [
+        ({ modulesPath, ... }: {
+          imports = [
+            "${modulesPath}/virtualisation/amazon-image.nix"
+          ];
+        })
+        ./configuration.nix
+        ({ ... }: { amazonImage.sizeMB = 16 * 1024; })
+      ];
+    };
+  }
 }
