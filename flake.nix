b1ed8d7faf467357a10b2603af526ed1fc05e65a
r33drichards <rwendt1337@gmail.com>
2025-01-09 13:04:11 -0800

    idek

--- flake.nix
+++ flake.nix
@@ -6,19 +6,18 @@
   };
 
   outputs = { nixpkgs, nixos-generators, ... }:
-  {
-    packages.reverse-proxy = nixos-generators.nixosGenerate {
-      system = "x86_64-linux";
-      format = "amazon";
-      modules = [
-        ({ modulesPath, ... }: {
-          imports = [
-            "${modulesPath}/virtualisation/amazon-image.nix"
+    {
+      reverse-proxy = nixpkgs.lib.nixosSystem
+        {
+          system = "x86_64-linux";
+          modules = [
+            ({ modulesPath, ... }: {
+              imports = [
+                "${modulesPath}/virtualisation/amazon-image.nix"
+              ];
+            })
+            ./configuration.nix
           ];
-        })
-        ./configuration.nix
-        ({ ... }: { amazonImage.sizeMB = 16 * 1024; })
-      ];
+        };
     };
-  };
 }
