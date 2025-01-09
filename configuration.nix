bbb7a252028ff35e095df0b0c3cdf497666cb649
r33drichards <rwendt1337@gmail.com>
2025-01-09 11:55:03 -0800

    add output for s3 bucket

--- configuration.nix
+++ configuration.nix
@@ -2,4 +2,15 @@
 
 {
   system.stateVersion = "23.05";
+
+  services.caddy = {
+    enable = true;
+    virtualHosts."nixos.jjk.is" = {
+      extraConfig = ''
+        respond "Hello World"
+      '';
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
--- k3s/ami-s3-bucket.tf
+++ k3s/ami-s3-bucket.tf
@@ -41,3 +41,8 @@ resource "aws_s3_bucket_public_access_block" "amis_public_access_block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+output "ami_bucket_name" {
+  description = "The name of the S3 bucket storing AMIs"
+  value       = aws_s3_bucket.amis.id
+}
