971c5d7bd3978e07fcbe43dec15f658c0841edcb
r33drichards <rwendt1337@gmail.com>
2025-01-09 12:46:10 -0800

    should wor

--- flake.nix
+++ flake.nix
@@ -1,19 +1,17 @@
+{ config, pkgs, ... }:
+
 {
-  description = "A very basic flake";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flakery.url = "github:getflakery/flakes";
-  };
-
-  outputs = { self, nixpkgs, flakery }: {
-
-    packages.x86_64-linux.reverse-proxy = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        flakery.nixosModules.flakery
-        ./configuration.nix
-      ];
-    };
+  packages.reverse-proxy = nixos-generators.nixosGenerate {
+    system = "x86_64-linux";
+    format = "amazon";
+    modules = [
+      ({ modulesPath, ... }: {
+        imports = [
+          "${modulesPath}/virtualisation/amazon-image.nix"
+        ];
+      })
+      ./configuration.nix
+      ({ ... }: { amazonImage.sizeMB = 16 * 1024; })
+    ];
   };
 }
