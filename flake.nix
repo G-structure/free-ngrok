e664a830792a8b0d55ce2f364229a2a1923875d7
r33drichards <rwendt1337@gmail.com>
2025-01-10 13:26:21 -0800

    vc

--- flake.nix
+++ flake.nix
@@ -5,30 +5,53 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-generators, ... }: {
-    nixosConfigurations.reverse-proxy = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, nixos-generators, flake-utils, ... }@inputs:
+    let
       system = "aarch64-linux";
-      modules = [
-        ./nixos-configuration.nix
-      ];
-    };
-
-    packages.x86_64-linux = {
-      reverse-proxy = nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-        format = "amazon";
-        modules = [
-          ({ modulesPath, ... }: {
-            imports = [
-              "${modulesPath}/virtualisation/amazon-image.nix"
-            ];
-          })
-          ./configuration.nix
-          ({ ... }: { amazonImage.sizeMB = 16 * 1024; })
-        ];
+    in
+    {
+      nixosConfigurations = {
+        test = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs.inputs = inputs;
+          modules = [
+            ({ modulesPath, ... }: {
+              imports = [
+                "${modulesPath}/virtualisation/qemu-vm.nix"
+              ];
+              virtualisation = {
+                graphics = false;
+                cores = 2;
+                memorySize = 2048;
+              };
+            })
+            ./configuration.nix
+          ];
+        };
       };
-    };
-  };
+    } // flake-utils.lib.eachDefaultSystem (host: {
+
+
+      packages.${host} = {
+        reverse-proxy = nixos-generators.nixosGenerate {
+          system = "aarch64-linux";
+          format = "amazon";
+          modules = [
+            ({ modulesPath, ... }: {
+              imports = [
+                "${modulesPath}/virtualisation/amazon-image.nix"
+              ];
+            })
+            ./configuration.nix
+            ({ ... }: { amazonImage.sizeMB = 16 * 1024; })
+          ];
+        };
+      };
+    });
 }
