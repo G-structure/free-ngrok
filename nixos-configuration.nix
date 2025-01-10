77bcdee4cff99166b524187cd8491353b5627880
r33drichards <rwendt1337@gmail.com>
2025-01-10 11:41:23 -0800

    found onlein

--- nixos-configuration.nix
+++ nixos-configuration.nix
@@ -1,26 +1,88 @@
-{ config, pkgs, ... }:
-
 {
-  system.stateVersion = "23.05";
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.dotfiles.orbstack;
 
-  # File system configuration
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-  };
+  inherit (config.dotfiles) username;
+  inherit (lib) mkForce;
+in
+{
+  options.dotfiles.orbstack.enable = lib.mkEnableOption "OrbStack specific configuration";
 
-  # Boot loader configuration for Apple Silicon
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  config = lib.mkIf cfg.enable {
 
-  services.caddy = {
-    enable = true;
-    virtualHosts."nixos.jjk.is" = {
-      extraConfig = ''
-        respond "Hello World"
-      '';
+    ### Orbstack-generated configuration
+    # Add OrbStack CLI tools to PATH
+    environment.shellInit = ''
+      . /opt/orbstack-guest/etc/profile-early
+
+      # add your customizations here
+
+      . /opt/orbstack-guest/etc/profile-late
+    '';
+
+    # Disable systemd-resolved
+    services.resolved.enable = false;
+    environment.etc."resolv.conf".source = "/opt/orbstack-guest/etc/resolv.conf";
+
+    # Faster DHCP - OrbStack uses SLAAC exclusively
+    networking.dhcpcd.extraConfig = ''
+      noarp
+      noipv6
+    '';
+    networking = {
+      # dhcpcd.enable = false;
+      useDHCP = false;
+      useHostResolvConf = false;
     };
-  };
 
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
+    # Disable sshd
+    services.openssh.enable = false;
+
+    # ssh config
+    programs.ssh.extraConfig = ''
+      Include /opt/orbstack-guest/etc/ssh_config
+    '';
+
+    # indicate builder support for emulated architectures
+    nix.settings.extra-platforms = [
+      "x86_64-linux"
+      "i686-linux"
+    ];
+
+    ### manual configuration
+    # turn off boot config
+    boot.plymouth.enable = mkForce false;
+    system.build.installBootLoader = mkForce "${pkgs.coreutils}/bin/true";
+
+    # turn off fs support
+    boot.supportedFilesystems = mkForce [ ];
+
+    # map the user to the macOS uid
+    users = {
+      users.${username} = {
+        uid = 501;
+        extraGroups = [ "wheel" ];
+
+        # simulate isNormalUser, but with an arbitrary UID
+        isSystemUser = mkForce true;
+        isNormalUser = mkForce false;
+        group = "users";
+        createHome = true;
+        home = "/home/${username}";
+        homeMode = "700";
+        useDefaultShell = true;
+      };
+      # This being `true` leads to a few nasty bugs, change at your own risk!
+      mutableUsers = false;
+    };
+    security.sudo.wheelNeedsPassword = false;
+
+    virtualisation.podman.enable = mkForce false;
+    services.unbound.enable = mkForce false;
+  };
 }
