69b54a84902eb77853f9c748061e88634c3a5748
r33drichards <rwendt1337@gmail.com>
2025-01-11 07:24:53 -0800

    test

--- configuration.nix
+++ configuration.nix
@@ -2,22 +2,18 @@
 
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-
-nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
   systemd.services.vscode-server = {
     description = "vscode serve-web";
-    after = [ "network-pre.target"  ];
-    wants = [ "network-pre.target"  ];
+    after = [ "network-pre.target" ];
+    wants = [ "network-pre.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig.Type = "simple";
+    serviceConfig.User = "robertwendt";
     script = ''
       ${pkgs.vscode}/bin/code serve-web --without-connection-token --host 0.0.0.0 --port 4321 --extensions-dir /home/alice/.vscode/extensions | ${pkgs.nix}/bin/nix run github:r33drichards/fix-vscode-server ${pkgs.nodejs}/bin/node
-      # useful for debugging
-      # ${pkgs.nix}/bin/nix run /home/alice/fix-vscode-server ${pkgs.nodejs}/bin/node
-
     '';
-    # serviceConfig.WorkingDirectory = "/home/alice";
+    serviceConfig.WorkingDirectory = "/Users/robertwendt";
     serviceConfig.Environment = "PATH=${pkgs.git}/bin:${pkgs.nix}/bin:/run/current-system/sw/bin:/usr/bin:/bin";
 
   };
