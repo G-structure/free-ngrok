d1648ccac5002dfedc917a79eb1ba70e34bf1ae1
r33drichards <rwendt1337@gmail.com>
2025-01-11 07:34:51 -0800

    k3s

--- configuration.nix
+++ configuration.nix
@@ -18,14 +18,48 @@
 
   };
 
-  services.caddy = {
-    enable = true;
-    virtualHosts."nixos.jjk.is" = {
-      extraConfig = ''
-        respond "Hello World"
-      '';
+
+
+  # enable k3s
+  networking.firewall.allowedTCPPorts = [
+    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+    # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+  ];
+  networking.firewall.allowedUDPPorts = [
+    # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+  ];
+  services.k3s.enable = true;
+  services.k3s.role = "server";
+  services.k3s.extraFlags = toString [
+    # "--kubelet-arg=v=4" # Optionally add additional args to k3s
+    "--write-kubeconfig-mode 644"
+  ];
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      # docker socket
+      dockerSocket.enable = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
     };
   };
+  networking.firewall.interfaces.podman0.allowedUDPPorts = [ 53 ];
 
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  # make nocodb depend on create-dir
+  virtualisation.oci-containers.backend = "podman";
+
+
+
 }
