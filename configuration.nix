8584f0b3b4a6147472eab50f12f3b5f2ecc7eb62
r33drichards <rwendt1337@gmail.com>
2025-01-11 07:14:37 -0800

    add vscode server

--- configuration.nix
+++ configuration.nix
@@ -2,6 +2,24 @@
 
 {
 
+  systemd.services.vscode-server = {
+    description = "vscode serve-web";
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "simple";
+    serviceConfig.User = "alice";
+    script = ''
+      ${pkgs.vscode}/bin/code serve-web --without-connection-token --host 0.0.0.0 --port 4321 --extensions-dir /home/alice/.vscode/extensions | ${pkgs.nix}/bin/nix run github:r33drichards/fix-vscode-server ${pkgs.nodejs}/bin/node
+      # useful for debugging
+      # ${pkgs.nix}/bin/nix run /home/alice/fix-vscode-server ${pkgs.nodejs}/bin/node
+
+    '';
+    serviceConfig.WorkingDirectory = "/home/alice";
+    serviceConfig.Environment = "PATH=${pkgs.git}/bin:${pkgs.nix}/bin:/run/current-system/sw/bin:/usr/bin:/bin";
+
+  };
+
   services.caddy = {
     enable = true;
     virtualHosts."nixos.jjk.is" = {
