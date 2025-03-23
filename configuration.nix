8c43632323e58c50e4efe9edeabed3698c83ca21
r33drichards <rwendt1337@gmail.com>
2025-03-22 20:39:34 -0700

    this should work in theory

--- configuration.nix
+++ configuration.nix
@@ -1,24 +1,25 @@
 { config, pkgs, ... }:
 
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nixpkgs.config.allowUnfree = true;
-    # set default editor to vim 
-  environment.variables = { EDITOR = "nvim"; };
-  programs.neovim.enable = true;
-  programs.neovim.defaultEditor = true;
- 
- services.frp.enable = true;
- services.frp.settings = {
-   serverPort = 7000;
- };
 
-  networking.firewall.allowedTCPPorts = [
-    7000
-  ];
-  networking.firewall.allowedUDPPorts = [
-    7000
-  ];
+  services.frp.enable = true;
+  services.frp.role = "server";
+  services.frp.settings = {
+     bindPort = 7000
+  };
+
+  # caddy revese proxy foo.example.com to 8080
+  services.caddy = {
+    enable = true;
+    config = ''
+      foo.flakery.xyz {
+        reverse_proxy 127.0.0.1:8080
+      }
+    '';
+  };
+
+  networking.firewall.allowedTCPPorts = [ 7000 80 443];
+  networking.firewall.allowedUDPPorts = [ 7000 80 443];
 
   nix.gc = {
     automatic = true;
