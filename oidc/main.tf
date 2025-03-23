6e1b0d8915610881e46c892911eee874e5ab95d0
r33drichards <rwendt1337@gmail.com>
2025-03-22 18:58:04 -0700

    more perms

--- oidc/main.tf
+++ oidc/main.tf
@@ -72,6 +72,11 @@ resource "aws_iam_policy" "github_actions" {
         Resource = [
           # EC2 resources
           "arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:*",
+          "arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:vpc/*",
+          "arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:subnet/*",
+          "arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:security-group/*",
+          "arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:route-table/*",
+          "arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:internet-gateway/*",
 
           # S3 bucket resources
           "arn:aws:s3:::*",
