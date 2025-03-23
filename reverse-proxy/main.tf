537a1ec843f5559a0d16cac6f765d0aa14af0492
r33drichards <rwendt1337@gmail.com>
2025-03-22 19:57:59 -0700

    ami

--- reverse-proxy/main.tf
+++ reverse-proxy/main.tf
@@ -35,7 +35,7 @@ module "k3s" {
 
   ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9tjvxDXYRrYX6oDlWI0/vbuib9JOwAooA+gbyGG/+Q robertwendt@Roberts-Laptop.local"
 
-  ami = "ami-05fbbcd01e13fe46b"
+  ami = "ami-0dfad4b8c58201573"
 }
 
 output "autoscaling_group_name" {
